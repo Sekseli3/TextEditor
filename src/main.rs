d426230f19402c34e23c75e5853b0f89a27aaec0
Sekseli3 <akseli.tuominen@aalto.fi>
2024-06-02 17:54:36 +0300

    movements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{stdout, Write};
 
 use crossterm::{cursor, event::read, terminal, ExecutableCommand, QueueableCommand};
-
+// this gives error match arms have imcompitable types, fix it
 fn main() -> anyhow::Result<()> {
     let mut cx = 0;
     let mut cy = 0;
@@ -15,6 +15,24 @@ fn main() -> anyhow::Result<()> {
         match read()? {
             crossterm::event::Event::Key(event) => match event.code {
                 crossterm::event::KeyCode::Char('q') => break,
+                //Fix this error:
+                crossterm::event::KeyCode::Char('h') => {
+                    if cx > 0 {
+                        cx -= 1
+                    } else {
+                        cx = 0
+                    }
+                }
+                crossterm::event::KeyCode::Char('j') => cy += 1,
+                crossterm::event::KeyCode::Char('k') => {
+                    if cy > 0 {
+                        cy -= 1
+                    } else {
+                        cy = 0
+                    }
+                }
+                crossterm::event::KeyCode::Char('l') => cx += 1,
+
                 _ => {}
             },
             _ => {}
