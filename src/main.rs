5c729613325a49051d01c8dd3bd49298fa2bc2ca
Sekseli3 <akseli.tuominen@aalto.fi>
2024-06-02 14:52:12 +0300

    cursor

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use std::io::stdout;
+use std::io::{stdout, Write};
 
-use crossterm::{
-    event::read,
-    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
-    ExecutableCommand,
-};
+use crossterm::{cursor, event::read, terminal, ExecutableCommand, QueueableCommand};
 
 fn main() -> anyhow::Result<()> {
+    let mut cx = 0;
+    let mut cy = 0;
     let mut stdout = stdout();
     terminal::enable_raw_mode()?;
     stdout.execute(terminal::EnterAlternateScreen);
     stdout.execute(terminal::Clear(terminal::ClearType::All))?;
-    read()?;
+    loop {
+        stdout.queue(cursor::MoveTo(cx, cy))?;
+        stdout.flush()?;
+        match read()? {
+            crossterm::event::Event::Key(event) => match event.code {
+                crossterm::event::KeyCode::Char('q') => break,
+                _ => {}
+            },
+            _ => {}
+        }
+    }
     stdout.execute(terminal::LeaveAlternateScreen);
     terminal::disable_raw_mode()?;
 
