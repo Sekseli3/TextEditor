8b922c22b1da77cb38ef898769f4245467901a11
Sekseli3 <akseli.tuominen@aalto.fi>
2024-06-02 13:42:25 +0300

    Blank screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdout;
+
+use crossterm::{
+    event::read,
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+
+fn main() -> anyhow::Result<()> {
+    let mut stdout = stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(terminal::EnterAlternateScreen);
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+    read()?;
+    stdout.execute(terminal::LeaveAlternateScreen);
+    terminal::disable_raw_mode()?;
+
+    Ok(())
 }
